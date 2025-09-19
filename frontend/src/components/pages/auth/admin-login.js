faab0f805c11ab624a504aa75b241291874b6a6d
Darshan S <149801799+Darshan980@users.noreply.github.com>
2025-09-19 19:10:12 +0530

    Delete frontend directory

--- frontend/src/components/pages/auth/admin-login.js
+++
@@ -1,6 +0,0 @@
-'use client';
-import LoginForm from '@/components/LoginForm';
-
-export default function AdminLoginPage() {
-  return <LoginForm isAdmin={true} />;
-}
--- frontend/src/components/pages/auth/login.js
+++
@@ -1,8 +0,0 @@
-
-// File: pages/auth/login.js (or src/pages/auth/login.js)
-'use client';
-import LoginForm from '@/components/LoginForm';
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
